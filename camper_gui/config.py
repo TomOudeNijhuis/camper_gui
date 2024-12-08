01a21db0946a07d06022aa4d6d204dc4ea37932d
Tom Oude Nijhuis <t.oudenijhuis@aemics.nl>
2024-12-08 12:08:42 +0100

    fix

--- camper_gui/config.py
+++ camper_gui/config.py
@@ -3,9 +3,18 @@ import platform
 
 
 class Settings(BaseSettings):
-    # api_base: str = "http://localhost:8000"
-    # api_base: str = "http://192.168.1.175:8000"
+    api_base: str = "http://localhost:8000"
+
+
+class DebugSettings(Settings):
     api_base: str = "http://192.168.68.167:8000"
 
 
-settings = Settings()
+class ProductionSettings(Settings):
+    pass
+
+
+if platform.machine() == "x86_64":
+    settings = DebugSettings()
+else:
+    settings = ProductionSettings()
